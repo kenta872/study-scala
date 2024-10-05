a42a58c471ab6e5918ebf2e886910d65a11306e4
NakaiKenta <nk19940709nk@gmail.com>
2024-10-05 21:53:20 +0900

    fix for scalafix

--- study-scala-app/app/filter/Filters.scala
+++ study-scala-app/app/filter/Filters.scala
@@ -6,5 +6,5 @@ import javax.inject.Inject
 
 class Filters @Inject()(globalLoggingFilter: GlobalLoggingFilter) extends HttpFilters {
     // ほかにFilter処理があればここに追加する
-    override val filters = Seq(globalLoggingFilter)
+    override val filters: Seq[GlobalLoggingFilter] = Seq(globalLoggingFilter)
 }
